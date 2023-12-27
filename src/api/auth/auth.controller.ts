47470435d8410d8f65cba88eb748ba3336789ef3
nagarajuthandu <nagaraju.thandu@fortunapix.com>
2023-12-27 22:40:30 +0530

    authentication updated

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,22 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete,Request } from '@nestjs/common';
+import { Controller, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService, private readonly jwtService: JwtService,private readonly configService: ConfigService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('login')
   async login(@Body() req: any) {
-    const user:any = this.authService.generateToken(req);
-    const payload = { sub: user._id, username: user.username }; 
-    const token =  this.jwtService.sign(payload,{
-      secret: this.configService.get<string>('jwt.secret'),
-      expiresIn: this.configService.get<string>('jwt.expiresIn'),
-    });
-    return {token}
-  }
+    try {
+      const user: any = await this.authService.checkUser(req);
+      if (!user || user === null) {
+        throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+      }
 
- 
+      const payload = { sub: user._id, username: user.username };
+      const token = this.jwtService.sign(payload, {
+        secret: this.configService.get('jwt.secret'),
+        expiresIn: this.configService.get('jwt.expiresIn'),
+      });
+
+      return { token };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException('Internal server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { User,UserDocument } from '../users/schema/user.schema';
+import { User, UserDocument } from '../users/schema/user.schema';
 import { Model } from 'mongoose';
-
 
 @Injectable()
 export class AuthService {
   constructor(@InjectModel(User.name) private readonly userModel: Model<UserDocument>) {}
-  async generateToken(req :any) {
-    return await this.userModel.findOne(req);
-    
+
+  async checkUser(req: any): Promise<UserDocument | null> {
+      const user = await this.userModel.findOne(req).exec();
+      if (!user || user===null) {
+        throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+      }
+      return user;
   }
 }
