eea69e47412ebbdaaf000668b553ddb894c1d22f
nagarajuthandu <nagaraju.thandu@fortunapix.com>
2023-12-29 17:07:23 +0530

    env file updated

--- src/config/env/database.config.ts
+++ src/config/env/database.config.ts
@@ -11,5 +11,14 @@ export default registerAs('database', () => ({
     authenticationDatabase: process.env.AUTHENTICATION_DATABASE || null,
     ssl: process.env.DATABASE_SSL === 'true' || false,
   },
-  production: {},
+  production: {
+    host: process.env.DATABASE_HOST || 'localhost',
+    srv: process.env.DATABASE_SRV === 'true' || false,
+    port: parseInt(process.env.DATABASE_PORT) || 27017,
+    name: process.env.DATABASE_NAME || 'my-app-db',
+    username: process.env.DATABASE_USERNAME || null,
+    password: process.env.DATABASE_PASSWORD || null,
+    authenticationDatabase: process.env.AUTHENTICATION_DATABASE || null,
+    ssl: process.env.DATABASE_SSL === 'true' || false,
+  },
 }));
